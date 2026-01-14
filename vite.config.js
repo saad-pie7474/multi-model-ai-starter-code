152bda715595a14eb821a108ec83dc516838bd8a
saad-pie7474 <rubinaali5432@gmail.com>
2026-01-14 17:59:13 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
+  // The base path must match your GitHub repository name
+  base: "/multi-model-ai-starter-code/", 
   plugins: [react(), tailwindcss()],
 });
